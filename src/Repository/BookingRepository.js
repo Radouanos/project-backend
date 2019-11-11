9171539e7be5cbca9162e1aca58abafb7a200ec9
ELBISSIS Radouane <redwan.b6@gmail.com>
2019-11-12 00:45:05 +0100

    ajouter bookingRepository without CI

---
+++ src/Repository/BookingRepository.js
@@ -0,0 +1,19 @@
+module.exports = class {
+    constructor(db) {
+        this.db = db;
+    }
+
+    create(booking)
+    {
+        if (!booking) {
+            return  'booking object is undefined';
+        }
+        if (!booking.jetPackId || !booking.startDate ||!booking.endDate) {
+            return  'booking object is missing information';
+        }
+        this.db
+            .get('bookings')
+            .push(booking.toJson())
+            .write()
+    }
+};
