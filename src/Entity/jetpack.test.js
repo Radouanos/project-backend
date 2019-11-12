572c5bfd30fe7ccaa976c917c0ddbdbf422a9e49
ELBISSIS Radouane <redwan.b6@gmail.com>
2019-11-12 01:10:35 +0100

    ajouter la fonction et les tests de GETAVAILABLE => la disponibilité

--- src/Entity/jetpack.test.js
+++ src/Entity/jetpack.test.js
@@ -1,4 +1,5 @@
 const Jetpack = require('./Jetpack');
+const Booking = require('../Repository/BookingRepository');
 const JetpackRepository=require('../Repository/JetpackRepository');
 describe('Jetpack toJson', function () {
 
@@ -101,4 +102,64 @@ describe('Jetpack Create',function()
             let jetpack = new JetpackRepository(dbMock);
             expect(jetpack.getSize()).toBe(1);
         });
+        test('Test getAvailable(..) with exception',() =>
+        {
+            const dataMockJetpack=
+                {
+                    get : jest.fn().mockReturnThis(),
+                    //find : jest.fn().mockReturnThis(),
+                    value : jest.fn().mockReturnValue([{ "id" : "4b499c2b-21d0-46d1-8078-964dfc4c3fcb" , "name": "ELBISSIS","image" : "SOSO.png"},{ "id" : "424f3a80-7b3c-4cd0-86d5-137d36fee747" , "name": "ELBISSIS","image" : "SOSO.png"}])
+                };
+            const dataMockBooking=
+                {
+                    get : jest.fn().mockReturnThis(),
+                    find : jest.fn().mockReturnThis(),
+                    value : jest.fn().mockReturnValue({"jetPackId" : "4b499c2b-21d0-46d1-8078-964dfc4c3fcb","startDate" : "2019-11-04" ,"endDate":"2019-11-05"})
+                }
+            let jetpack = new JetpackRepository(dataMockJetpack);
+            let booking = new Booking(dataMockBooking);
+            expect(jetpack.getAvailable('2019-11-054','2019-11-05',booking)).toBe('Invalid date format');
+        });
+        test('Test getAvailable(..) without Exception',() =>
+        {
+            const dataMockJetpack=
+                {
+                    get : jest.fn().mockReturnThis(),
+                    Object : jest.fn().mockReturnThis(),
+                    keys : jest.fn().mockReturnThis(),
+                    length : jest.fn().mockReturnValue(2),
+                    value : jest.fn().mockReturnValue([{ "id" : "424f3a80-7b3c-4cd0-86d5-137d36fee747" , "name": "ELBISSIS","image" : "SOSO.png"}])
+                };
+            const dataMockBooking=
+                {
+                    get : jest.fn().mockReturnThis(),
+                    find : jest.fn().mockReturnThis(),
+                    value : jest.fn().mockReturnValue(undefined)
+                }
+            let jetpack = new JetpackRepository(dataMockJetpack);
+            let booking = new Booking(dataMockBooking);
+            expect(jetpack.getAvailable('2019-11-01','2019-11-05',booking)).toEqual([{ "id" : "424f3a80-7b3c-4cd0-86d5-137d36fee747" , "name": "ELBISSIS","image" : "SOSO.png"}]);
+        });
+
+        test('Test getAvailable(..) NO available jetpack',() =>
+        {
+            const dataMockJetpack=
+                {
+                    get : jest.fn().mockReturnThis(),
+                    Object : jest.fn().mockReturnThis(),
+                    keys : jest.fn().mockReturnThis(),
+                    length : jest.fn().mockReturnValue(2),
+                    value : jest.fn().mockReturnValue([{ "id" : "424f3a80-7b3c-4cd0-86d5-137d36fee747" , "name": "ELBISSIS","image" : "SOSO.png"}])
+                };
+            const dataMockBooking=
+                {
+                    get : jest.fn().mockReturnThis(),
+                    find : jest.fn().mockReturnThis(),
+                    value : jest.fn().mockReturnValue([{"jetPackId": "424f3a80-7b3c-4cd0-86d5-137d36fee747","startDate": "2019-11-01", "endDate": "2019-11-05"}])
+                }
+            let jetpack = new JetpackRepository(dataMockJetpack);
+            let booking = new Booking(dataMockBooking);
+            expect(jetpack.getAvailable('2019-11-01','2019-11-05',booking)).toEqual([]);
+        });
+
     });
